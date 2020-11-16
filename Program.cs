612bca0e9213df43568414a8b85d4c0ff43def62
Antoni <antoni.bonacic@icloud.com>
2020-11-16 23:37:30 +0100

    test4234

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace Kino_projekt
 {
     struct Pretplata
-    { //test1234567
+    { //ovo je jos jedan test
         public string kupac;
         public int godina;
         public double uplata;
